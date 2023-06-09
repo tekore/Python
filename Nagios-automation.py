6383f139a208f37d24146db1b0861c25418b0cf6
tekore <tekore@tutamail.com>
2023-06-09 14:58:50 +0100

    Nagios-automation.py updates

--- Nagios-automation.py
+++ Nagios-automation.py
@@ -3,14 +3,16 @@ from queue import Queue
 from subprocess import DEVNULL
 
 class Prerun:
-    def __init__(self, monitoring_user, monitoring_secret, fixable_alerts):
+    def __init__(self, domain, monitoring_user, monitoring_secret, port, fixable_alerts):
+        self.domain = domain
         self.monitoring_user = monitoring_user
         self.monitoring_secret = monitoring_secret
+        self.port = port
         self.fixable_alerts = fixable_alerts
 
     def get_dump(self):
         print("Downloading JSON monitoring dump...")
-        mprocess = subprocess.Popen("sshpass -f .p.pfile ssh -q -o StrictHostKeyChecking=no <DOMAIN>\\\\%s@<NAGIOS_HOST>.<DOMAIN> 'rm -f ./dump.* && wget -O dump.monitoring --no-check-certificate \"https://<NAGIOS_HOST>.<DOMAIN>:<PORT>/<REDACTED>?view_name=<REDACTED>&output_format=JSON&_username=%s&_secret=%s\" && cat dump.*' > mondump.json" %(args.u, self.monitoring_user, self.monitoring_secret), stdout=subprocess.DEVNULL,stderr=DEVNULL, shell=True, bufsize=0)
+        mprocess = subprocess.Popen("./sshpass -f .p.pfile ssh -q -o StrictHostKeyChecking=no %s\\\\$USER@<NAGIOS_HOST> 'rm -f ./dump.* && wget -O dump.monitoring --no-check-certificate \"https://<NAGIOS_HOST>:%s<REDACTED>&output_format=JSON&_username=%s&_secret=%s\" && cat dump.*' >mondump.json" %(self.domain, self.port, self.monitoring_user, self.monitoring_secret), stdout=subprocess.DEVNULL,stderr=DEVNULL, shell=True, bufsize=0)
         mprocess.wait()
         print("Monitoring JSON dump has been downloaded.\nSearching for the Monitoring dump in the current directory..")
         p = [i for i in os.listdir("./") if i.endswith(".json")]
@@ -32,7 +34,7 @@ class Prerun:
 
     def pre_summary(self, dict_list):
         file = open("Prerun_Summary_%s" %(time.strftime("%H:%M:%S", time.localtime())), "w+")
-        file.write("### Monitoring Script Prerun Summary ###\n\nThe following stores will be accessed.")
+        file.write("### Monitoring Script Prerun Summary ###\n\nThe following hosts will be SSH'ed to.")
         for i in dict_list:
                 if args.critonly is True and re.search("CRIT", i.get("Detail")) and i['Alert'] in fixable_alerts:
                     file.write("\n%s\n- %s: %s %s" %(i['Store'], i['Host'], i['Alert'], i['Detail']))
@@ -43,7 +45,6 @@ class Prerun:
         Postrun.cleanup(self)
 
     def pre(self):
-        print("### Monitoring Script ###")
         self.pfile()
         dict_list = self.build_dict()
         alerts = list(set([i['Alert'] for i in dict_list]))
@@ -55,7 +56,7 @@ class Prerun:
                 print(a,"-", len([i for i in dict_list if i['Alert'] == a and (re.search("CRIT", i['Detail']))]))
             else:
                 print(a,"-", len([i for i in dict_list if i['Alert'] == a]))
-        print("\nThis script will now cycle through the stores and clear the above alerts.\n")
+        print("\nThis script will now cycle through the hosts and clear the above alerts.\n")
         try:
             for i in range(9,0,-1):
                 print("Count down:", i, end='\r')
@@ -94,9 +95,17 @@ class Store:
     def cdump(self):
         self.pin.write("cd <REDACTED>\n ls -la\n rm -r ./<REDACTED>\n ls -la\n")
 
+    def pool(self):
+        self.pin.write("<REDACTED>\n")
+
+    def state(self):
+        self.pin.write("systemctl restart <REDACTED>.service\n")
+
     def makestore(self):
-        n = re.split("<DOMAIN>j-|-\D", self.store, maxsplit=2)
-        sshstore = self.host.strip('"')+".s"+n[1]
+        n = self.store.split("-", 3)
+        if len(n[2]) > 1:
+            n[2] = '1'
+        sshstore = self.host.strip('"')+".s"+n[1]+"-"+n[2]
         return(sshstore)
 
     def sudo(self, process):
@@ -114,7 +123,7 @@ class Store:
                     self.pin.write("whoami\n")
                 if re.search(r"root@", line):
                     return(process)
-        raise RuntimeError("Sudo Timeout for store: %s" %(self.store))
+        raise RuntimeError("Sudo Timeout for host: %s" %(self.store))
 
     def connect(self):
         with lock:
@@ -127,38 +136,45 @@ class Store:
         master, slave = pty.openpty()
         self.pin = os.fdopen(master, "w")
         print("\nInitialising SSH connection to %s:" %(store))
-        process = subprocess.Popen('sshpass -f .p.pfile ssh -o ConnectTimeout=60 -o StrictHostKeyChecking=no <DOMAIN>\\\\%s@%s' %(args.u,store),stdin=slave,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True, universal_newlines=1, bufsize=0)
+        process = subprocess.Popen('./sshpass -f .p.pfile ssh -o ConnectTimeout=60 -o StrictHostKeyChecking=no %s\\\\%s@%s' %(domain,args.u,store),stdin=slave,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True, universal_newlines=1, bufsize=0)
         try:
             process.stdout.readlines(1)[0]
         except:
             errorhosts.append(store)
             process.terminate()
             self.clean()
-            return(print("ERROR: Unable to connect to store: %s." %(store)))
-        print("SSH Successful for store: %s.\nSudoing up for store: %s" %(store,store))
+            return(print("ERROR: Unable to connect to host: %s." %(store)))
+        print("SSH Successful for host: %s.\nSudoing up for host: %s" %(store,store))
         try:
             self.sudo(process)
         except RuntimeError:
-            skippedhosts.append("%s %s %s" %(self.store,self.host,self.alert))
+            skippedhosts.append("%s %s" %(store,self.alert))
             process.terminate()
             self.clean()
-            return(print("SUDO TIMEOUT! for store: %s\nStore didn't sudo up within 30 seconds. Skipping Store." %(store)))
-        print("Sudo successful for store: %s. Running commands.." %(store))
+            return(print("Sudo timeout for host: %s\nHost didn't sudo up within 30 seconds. Skipping host." %(store)))
+        print("Sudo successful for host: %s.\nRunning commands.." %(store))
         menu = {'<REDACTED>' : self.ddata,
                 '<REDACTED>' : self.wagent,
                 'fs_/var/log' : self.varlog,
                 '<REDACTED>' : self.cdump,
-                '<REDACTED>' : self.pd
+                '<REDACTED>' : self.pd,
+                '<REDACTED>' : self.pool,
+                '<REDACTED>' : self.state
                }
         menu[self.alert]()
         self.pin.write("exit\nexit\n")
         file = open("Debug-output", "a")
-        while args.debug:
-            line = process.stdout.readline()
-            file.write(line)
-            if not line:
-                break
-        print("Success! Closing SSH session for store %s" %(store))
+        if args.debug:
+            timeout = time.time() + 30
+            y = select.poll()
+            y.register(process.stdout,select.POLLIN)
+            while time.time() < timeout:
+                while y.poll(1000):
+                    line = process.stdout.readline()
+                    file.write(line)
+                    if not line:
+                        break
+        print("Success! Closing SSH session for host %s" %(store))
         process.communicate()
         donehosts.append("%s:  %s" %(store,self.alert))
         self.clean()
@@ -173,7 +189,7 @@ class Postrun:
         files.append(file.name)
         file.write("### Monitoring Script Postrun Summary ###\n")
         if errorhosts:
-            file.write("\nUnable to SSH to the following stores:\n")
+            file.write("\nUnable to SSH to the following hosts:\n")
             for r in errorhosts:
                 file.write("- %s\n" %(r))
         if donehosts:
@@ -181,7 +197,7 @@ class Postrun:
             for i in donehosts:
                 file.write("- %s\n" %(i))
         if skippedhosts:
-            file.write("\nSkipped the following hosts:\n")
+            file.write("\nSkipped the following alerts:\n")
             for i in skippedhosts:
                 file.write("- %s\n" %(i))
         file.close()
@@ -197,7 +213,7 @@ class Postrun:
     def cleanup(self):
         sys.stdout.flush()
         for i in os.listdir("./"):
-            if i.endswith(".pfile"):
+            if i.endswith(".pfile") or i.endswith(".running"):
                 os.remove(i)
         sys.exit()
 
@@ -218,14 +234,53 @@ def buildqueue():
         if args.critonly is False and i['Alert'] in fixable_alerts:
             queue.put(i, block=True, timeout=None)
 
+def get_domain():
+    print("### Monitoring Script ###")
+    with open('/etc/hosts', 'r') as dfile:
+        for line in dfile:
+            for i in domains:
+                if i in line:
+                    print("Domain Detected:", domains[i]["domain"])
+                    return(domains[i]["domain"],domains[i]["user"],domains[i]["secret"],domains[i]["port"])
+        print("Unable to detect environment, please update the script for this environment. \n\nCurrent supported environments:")
+        for i in domains:
+            print("- ",i)
+        Postrun().cleanup()
+
+def prechecks():
+    if os.geteuid() == 0:
+        sys.exit(print("Do NOT run this script as root, please run it as your user."))
+    if "sshpass" not in os.listdir("./"):
+        sys.exit(print("This script relies on sshpass being in the current directory, please copy sshpass to %s and ensure it's named 'sshpass'" %(os.getcwd())))
+    for i in os.listdir("./"):
+        if i.endswith(".running"):
+            sys.exit(print("Someone is already running this script!\nIf you are 100% sure noone is running this script and this is infact an error, remove the 'RUNNING.running' file in:", os.getcwd()))
+        if i.startswith("Debug-output") or i.startswith("mondump"):
+            os.remove(i)
+    file = open('RUNNING.running', 'w')
+
 if __name__ == '__main__':
     args = args()
-    monitoring_user = "<REDACTED>"
-    monitoring_secret = "<REDACTED>"
-    fixable_alerts = ['<REDACTED>', 'fs_/var/log', '<REDACTED>', '<REDACTED>']
-    password = getpass.getpass('Enter your password to enable the monitoring dump to be downloaded:')
+    prechecks()
+    domains = {
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"},
+     "<REDACTED>": {"domain": "<REDACTED>", "user": "<REDACTED>", "secret": "<REDACTED>", "port": "<REDACTED>"}
+    }
+    fixable_alerts = ['<REDACTED>', 'fs_/var/log', '<REDACTED>', '<REDACTED>', '<REDACTED>', '<REDACTED>']
+    domain, monitoring_user, monitoring_secret, port = get_domain()
+    try:
+        password = getpass.getpass('Enter your password to use with SSH:')
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt, Exiting..")
+        Postrun().cleanup()
     donehosts, skippedhosts, errorhosts, files, jobs, queue, lock, sema = [], [], [], [], [], Queue(), threading.Lock(), threading.Semaphore(value=args.threads)
-    dict_list, alerts = Prerun(monitoring_user, monitoring_secret, fixable_alerts).pre()
+    dict_list, alerts = Prerun(domain, monitoring_user, monitoring_secret, port, fixable_alerts).pre()
     buildqueue()
     while not queue.empty():
         try:
@@ -237,7 +292,7 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             print("KeyboardInterrupt, Exiting..")
             Postrun().cleanup()
-    print("### Waiting for all threads to complete their tasks ###")
-    for i in jobs:
-        i.join()
+    while any(i.is_alive() for i in jobs):
+        print("\n### Waiting for all threads to complete their tasks ###\n")
+        time.sleep(5)
     Postrun().summary()

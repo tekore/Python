552387f3d3c3e76afbc87d6c957923af850319fb
te <tekore@tutamail.com>
2023-10-22 05:43:30 +0200

    Cleaned up and started WebApp project

---
+++ Webapp/README.md
@@ -0,0 +1,6 @@
+# Flask WebApp
+
+This is a work in progress.
+
+## Notes
+Flask defaults to the template folder if another folder for the HTML templates are not specified.
---
+++ Webapp/webapp.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
